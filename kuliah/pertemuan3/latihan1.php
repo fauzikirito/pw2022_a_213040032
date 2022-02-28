1b29723560f6101b73dd308ce796fe80377ba2f2
Fauzi Kirito <fauziilyasfin@gmail.com>
2022-02-28 13:44:39 +0700

    Mengerjakan Tugas Latihan Pertemuan 3

--- kuliah/pertemuan3/latihan1.php
+++ kuliah/pertemuan3/latihan1.php
@@ -1,10 +1,30 @@
 <?php
-//Pengulangan
+//PENGULANGAN
 //for
 //while
 //do - while
-//foreach
+//foreach : Pengulangan Khusus Array
 
+for($i = 0; $i < 5; $i++) {
+    echo "Hello World! <br>";
+}
+
+echo "<hr>";
+
+$i = 0;
+while($i < 5) {
+    echo "Hello World! <br>";
+    $i++;
+}
+
+echo "<hr>";
+
+//pengulangan do while akan menjalankan terlebih dahulu, walau kondisi false sekalipun
+$i = 10;
+do {
+    echo "Hello World! <br>";
+    $i++;
+} while($i < 5);
 
 
 ?>
