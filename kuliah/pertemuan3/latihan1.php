99df7534e17df9843975421d884d1f4f8e757260
Fauzi Kirito <fauziilyasfin@gmail.com>
2022-02-28 14:36:15 +0700

    Mengerjakan Latihan Pengkondisian

--- kuliah/pertemuan3/latihan1.php
+++ kuliah/pertemuan3/latihan1.php
@@ -26,5 +26,53 @@ do {
     $i++;
 } while($i < 5);
 
+echo "<hr>";
+
 
 ?>
+
+<!-- Pengulangan pada HTML Cara 1 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php
+            for($i = 1; $i <= 3; $i++) {
+                echo "<tr>";
+                for($j = 1; $j <= 5; $j++) {
+                    echo "<td>$i, $j</td>";
+                }
+            } echo "</tr>";
+
+        ?>
+    </table>
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <?php for($i = 1; $i <= 3; $i++) { ?>
+            <tr>
+                <?php for($j = 1; $j <= 5; $j++) { ?>
+                    <td><?php echo "$i, $j" ?></td>
+                <?php } ?>
+            </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,26 @@
+<?php
+//PENGKONDISIAN/PERCABANGAN
+//if else
+//if else if else
+//ternary
+//switch
+
+
+//Penggunaan if else
+$x = 10;
+if($x < 10) {
+    echo "Benar";
+} else {echo "Salah";
+}
+
+echo "<hr>";
+
+//Pengunaan if else if else
+$x = 20;
+if($x < 10) {
+    echo "Benar";
+} else if($x == 20) {
+    echo "Bingo!";
+} else {echo "Salah";
+}
+?>
