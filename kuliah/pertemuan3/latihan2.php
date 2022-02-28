d247d1a33c166d569d8b16d197d7d5d561f5584b
Fauzi Kirito <fauziilyasfin@gmail.com>
2022-02-28 15:17:45 +0700

    Menambahkan switch dan ternary

--- kuliah/pertemuan3/latihan2.php
+++ kuliah/pertemuan3/latihan2.php
@@ -21,6 +21,38 @@ if($x < 10) {
     echo "Benar";
 } else if($x == 20) {
     echo "Bingo!";
-} else {echo "Salah";
+} else { echo "Salah";
+}
+
+echo "<hr>";
+
+//Switch
+$angka = 2;
+switch ($angka) {
+    case 1:
+        echo "Isi variabel angka adalah 1";
+        break;
+    case 2:
+        echo "Isi variabel angka adalah 2";
+        break;
+    case 3:
+        echo "Isi variabel angka adalah 3";
+        break;
+    default:
+        echo "Variabel angka tidak ditemukan";
+        break;
+}
+
+echo "<hr>";
+
+//Ternary
+$nilai = 10;
+echo $nilai > 7 ? "Sangat Baik" : "Baik";
+//Apabila menuliskan menggunakan struktur if else
+$nilai = 10;
+if($nilai > 5) {
+    echo "Sangat Baik";
+} else {
+    echo "Baik";
 }
 ?>
