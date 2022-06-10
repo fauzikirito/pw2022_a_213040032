37650be12afe557b389007c7764f8f6ce82fd4fc
Fauzi Kirito <fauziilyasfin@gmail.com>
2022-06-11 06:33:13 +0700

    Menambahkan file sql Database

--- tubes/Database/Database Tubes.sql
+++ tubes/Database/Database Tubes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2022 at 12:59 AM
+-- Generation Time: Jun 11, 2022 at 01:31 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -39,7 +39,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id_admin`, `nama_admin`, `username`, `password`) VALUES
-(1, 'Fauzi Ilyas Nuryadi', 'admin', '821c555bd3c5c9b949c00722bd993f45'),
+(1, 'Fauzi Ilyas Nuryadi', 'admin', '827ccb0eea8a706c4c34a16891f84e7b'),
 (8, 'Kiritod', 'kirito', '0f1d8ced8b8821476ba4f8db915df1d1');
 
 -- --------------------------------------------------------
@@ -63,12 +63,12 @@ CREATE TABLE `produk` (
 --
 
 INSERT INTO `produk` (`id_produk`, `nama_produk`, `harga_produk`, `deskripsi_produk`, `gambar_produk`, `status_produk`, `date_created`) VALUES
-(16, 'Logo', 5000, '<p>Logo Terbaik</p>', 'Logo.jpg', 1, '2022-06-01 10:46:40'),
-(41, 'Crop', 120000, '<p>Duar!</p>', 'produk1654076183.png', 1, '2022-06-01 11:09:49'),
-(42, 'Figure Kirito', 1500000, '<p>Figure Kirito</p>', 'produk1654076502.jpeg', 1, '2022-06-01 09:53:16'),
+(42, 'Figure Kirito Alicization', 1500000, '<p>Classification: EXQ Figure<br />Origin: Sword Art Online: Alicization<br />Character: Kirito<br />Company: Banpresto As Manufacturer<br />Materials: ABS, PVC<br />Scale &amp; Dimensions: H=23cm</p>', 'produk1654076502.jpeg', 1, '2022-06-10 23:16:44'),
 (43, 'Action Figure Saber Fate', 1000000, '<p>Model : Action Figure PVC 1/7 Scale Alter Fate Stay Night Saber Dress Code<br />Material : PVC<br />Detail &amp; Cat : High Quality<br />Tinggi : 24 cm<br />Kemasan : Box<br />Ketersediaan Stok : Terbatas ( Grab it Fast )<br />Packing : Free Bubblewrap + Kardus Pelapis<br />PRODUK RECAST/TIDAK ORIGINAL<br /><br /><br />NOTE : barang ready siap kirim , packing aman kualitas detail dan warna terjamin OK !!!<br />&nbsp;</p>', 'produk1654256326.jpg', 1, '2022-06-03 11:38:46'),
-(44, 'dawda', 111, '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque massa lorem, ut mattis lacus tincidunt vitae. Phasellus ullamcorper urna et enim auctor commodo. Vivamus id vestibulum dolor. Duis bibendum tristique augue. Vestibulum varius ornare risus, sed ornare leo faucibus vitae. Integer aliquam enim sit amet magna mollis, ut semper quam convallis. Vivamus non imperdiet nunc. Proin vitae arcu at felis iaculis porta viverra ut ante. Vivamus tempor tellus eleifend commodo vestibulum. Quisque finibus congue velit sagittis ornare. Pellentesque mollis libero nulla, et hendrerit felis congue id. Nunc feugiat blandit luctus. Cras velit urna, lobortis ac accumsan at, egestas eu ligula.</p><p>Nam bibendum faucibus rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque ultrices tellus enim, in ornare tortor vulputate et. Fusce quis est posuere lectus bibendum convallis. Nulla convallis suscipit enim sit amet malesuada. Curabitur rhoncus diam a faucibus consectetur. Nullam faucibus eros id ante imperdiet aliquam.</p>', 'produk1654236909.png', 1, '2022-06-03 10:50:32'),
-(45, 'adw', 10, '<p>dawda</p>', 'produk1654605584.jpg', 1, '2022-06-07 12:39:44');
+(45, 'Action Figure Saber Fate Dress Code', 1200000, '<p>Model : Action Figure PVC 1/7 Scale Alter Fate Stay Night Saber Dress Code<br />Material : PVC<br />Detail &amp; Cat : High Quality<br />Tinggi : 24 cm<br />Kemasan : Box<br />Ketersediaan Stok : Terbatas ( Grab it Fast )<br />Packing : Free Bubblewrap + Kardus Pelapis<br />Original</p>', 'produk1654605584.jpg', 1, '2022-06-10 23:15:50'),
+(46, 'Action Figure Kirito Ordinal Scale', 8500000, '<p>Material : PVC<br />Detail : High Quality<br />Tinggi : 25 cm<br /><strong>Original</strong></p>', 'produk1654903079.jpg', 1, '2022-06-10 23:24:37'),
+(47, 'Figure Kirito GGO Season 2', 8500000, '<p>Kondisi: Baru<br />Berat: 813 Gram<br />Waktu Preorder: 30 Hari<br />Kategori: Action Figure<br />Etalase: Action Figure</p><p>From Medicom Toy. The hero of Sword Art Online 2 becomes a Real Action Hero! Kirito appears as he did in the Gun Gale Online game during the series&#39; second season with a realistic costume, photon sword (with replaceable clear parts), FN Five Seven pistol, multiple hands and facial expressions and a movable stand!.</p>', 'produk1654903239.jpg', 1, '2022-06-10 23:20:39'),
+(48, 'Action Figure Asuna (Aincrad Ver)', 5000000, '<p>Kondisi: Baru<br />Berat: 500 Gram<br />Kategori: Action Figure<br />Etalase: Semua Etalase<br />Material : PVC<br />Tinggi : 25 cm<br />Barang Original</p>', 'produk1654903429.jpg', 1, '2022-06-10 23:23:49');
 
 --
 -- Indexes for dumped tables
@@ -100,7 +100,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
